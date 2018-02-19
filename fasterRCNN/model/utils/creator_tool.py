15ff341f17b7ae4922fa5a36666d9e531c6d38a9
yrowe <912431483@qq.com>
2018-02-19 23:06:28 +0800

    ProposalTargetCreator

--- fasterRCNN/model/utils/creator_tool.py
+++ fasterRCNN/model/utils/creator_tool.py
@@ -122,3 +122,126 @@ class ProposalCreator:
 		return roi
 
 
+class ProposalTargetCreator(object):
+	'''
+	Assign ground truth bounding boxes to given RoIs.
+
+	`__call__` of this class generates training targets
+	for each object proposal.
+	This is used to train Faster RCNN.
+
+
+	Args:
+		n_sample(int): The number of sampled regions.
+		pos_ratio(float): Fraction of regions that is labeled as a foreground.
+		pos_iou_thresh(float): IoU threshold for a RoI to be
+			considered as a forground.
+		neg_iou_thresh_upper(float): the upper threshold for a RoI to be 
+			considered as a background.
+		neg_iou_thresh_lower(float): the lower threshold for a RoI to be 
+			considered as a background.
+
+			So this RoI that are used as a negative sample. their IoU should in
+				[`neg_iou_thresh_lower`, `neg_iou_thresh_upper`]
+
+	'''
+
+	def __init__(self, n_sample=128,pos_ratio=0.25,pos_iou_thresh=0.5,
+					neg_iou_thresh_upper=0.5, neg_iou_thresh_lower=0.):
+		self.n_sample = n_sample
+		self.pos_ratio = pos_ratio
+		self.pos_iou_thresh = pos_iou_thresh
+		self.neg_iou_thresh_upper = neg_iou_thresh_upper
+		self.neg_iou_thresh_lower = neg_iou_thresh_lower
+
+	def __call__(self, roi, bbox, label, 
+				 loc_normalize_mean=(0.,0.,0.,0.),
+				 loc_normalize_std=(0.1,0.1,0.2,0.2)):
+		'''
+		Assign the ground truth bounding boxes to anchors.
+
+		The function samples total of `self.n_sample` RoIs
+		from the combination of `roi` and `bbox`.
+
+		The RoIs are assigned with the ground truth class labels
+		as well as bounding box offsets and scales to match the ground
+		truth bounding boxes. As many as `pos_ratio * self.n_sameple`RoIs
+		are sampled as foregrounds.
+
+		Offsets and scales of bounding boxes are calculated using
+		`model.utils.bbox_tools.bbox2loc`
+		Also, types of input arrays and output arrays are the same.
+
+		some notations:
+		math:`S` is the total number of sampled RoIs, which equals `self.n_sample`.
+		math:`L` is the number of object classes possibly including the background.
+
+		Args:
+			roi(array): Region of Interests(RoIs) from which we sample.
+				Its shape is `(R, 4)`
+			bbox(array): The coordinates of ground truth bounding boxes.
+				Its shape is `(R', 4)`
+			label(array): Ground truth bounding box labels.Its shape is `(R', )
+				range is `[0, L-1]`,where `L` is the number of foreground classes.
+			loc_normalize_mean(tuple of four floats): Mean values to 
+				normalize cooridinates of bounding boxes.
+			loc_normalize_std(tuple of four floats): Standard deviation of the coordinates
+				of bounding boxes.
+
+		returns:
+			(array, array, array):
+
+			sample_roi: Regions of interests that are sampled.
+				Its shape is `(S, 4)`
+			gt_roi_loc: Offsets and scales to match the sampled RoIs to
+				the ground truth bounding boxes.
+				Its shape is `(S, 4)`
+			gt_roi_label: Labels assigned to sampled RoIs.Its shape is
+				`(S, )`.Its range is `[0, L]`.the label value 0 refers background.
+
+		'''
+
+		n_bbox, _ = bbox.shape
+
+		roi = np.concatenate((roi, bbox), axis=1)
+
+		pos_roi_per_image = np.round(self.n_sample * self.pos_ratio)
+		iou = bbox_iou(roi, bbox)
+		gt_assignment = iou.argmax(axis=1)
+		max_iou = iou.max(axis=1)
+
+		# give bias 1 for each foreground label. so label = 0 represents foreground.
+		gt_roi_label = label[gt_assignment] + 1
+
+		#select foreground RoIs as those with >= pos_iou_thresh IoU.
+		pos_index = np.where(max_iou >= self.pos_ious_thresh)[0]
+		pos_roi_per_this_image = int(min(pos_roi_per_image, pos_index.size))
+		if pos_index.size > 0:
+			pos_index = np.random.choice(
+				pos_index, size=pos_roi_per_image, repalce=False)
+
+		#select background RoIs as those IoU in
+		# [neg_iou_thresh_lower, neg_iou_thresh_upper).
+		neg_index = np.where((max_iou < self.neg_iou_thresh_upper)&
+							 (max_iou >= self.neg_iou_thresh_lower))[0]
+		neg_roi_per_this_image = self.n_sample - pos_roi_per_this_image
+		neg_roi_per_this_image = int(min(neg_roi_per_this_image, 
+										neg_index.size))
+		if neg_index.size > 0:
+			neg_index = np.random.choice(
+					neg_index, size=neg_roi_per_this_image, replace=False)
+
+
+		#The indices that we're selecting (both positive and negative).
+		# ? make sure `keep_index` == 128 always true ? prove?
+ 		keep_index = np.append(pos_index, neg_index)
+ 		gt_roi_label = gt_roi_label[keep_index]
+ 		gt_roi_label[pos_roi_per_this_image:] = 0  # negative label->0
+ 		sample_roi = roi[keep_index]
+
+ 		#Compute offsets and scales to match sampled RoIs to the GTs.
+ 		# !TODO bbox2loc bbox_tools.py
+ 		gt_roi_loc = bbox2loc(sample_roi, bbox[gt_assignment[keep_index]])
+ 		gt_roi_loc = ((gt_roi_loc - np.array(loc_normalize_mean, np.float32)
+ 					)/np.array(loc_normalize_std, np.float32))
+ 		return sample_roi, gt_roi_loc, gt_roi_label
