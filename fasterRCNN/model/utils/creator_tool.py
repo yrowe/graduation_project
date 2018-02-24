fc924831ac97fdd3248d5be893a4701763f17657
yrowe <912431483@qq.com>
2018-02-24 10:49:47 +0800

    anchorTargetCreator func

--- fasterRCNN/model/utils/creator_tool.py
+++ fasterRCNN/model/utils/creator_tool.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import cupy as cp 
 
-from model.utils.bbox_tools import loc2bbox
+from model.utils.bbox_tools import loc2bbox, bbox2loc, bbox_iou
 from model.utils.nms import non_maximum_suppression
 
 class ProposalCreator:
@@ -245,3 +245,135 @@ class ProposalTargetCreator(object):
  		gt_roi_loc = ((gt_roi_loc - np.array(loc_normalize_mean, np.float32)
  					)/np.array(loc_normalize_std, np.float32))
  		return sample_roi, gt_roi_loc, gt_roi_label
+
+
+
+class AnchorTargetCreator(object):
+	'''
+	Assign the ground truth bounding boxes to anchors in the process of training rpn.
+
+	Assigns the ground truth bounding boxes to anchors for training Region Proposal Networks introduced in Faster_RCNN.
+
+	Offsets and scales to match anchors to the ground truth are calculated using the encoding of `model.utils.bbox_tools.bbox2loc`
+
+	Args:
+		n_sample(int): The number of regions to produce.
+		pos_iou_thresh(float): Anchors with IoU above this threshold will be assigned as positive.
+		neg_iou_thresh(float): Anchors with IoU below this threshold will be assigned as negative.
+		pos_ratio(float): Ratio of positive regions in the sampled regions.
+
+	'''
+
+	def __init__(self, n_sample=256, pos_iou_thresh=0.7, neg_iou_thresh=0.3, pos_ratio=0.5):
+		self.n_sample = n_sample
+		self.pos_iou_thresh = pos_iou_thresh
+		self.neg_iou_thresh = neg_iou_thresh
+		self.pos_ratio = pos_ratio
+
+	def __call__(self, bbox, anchor, img_size):
+		'''
+		assign ground truth supervision to sampled subset of anchors.
+
+		Type of input array and output array are same.
+
+		Notations:
+			`S`: number of anchors.
+			`R`: number of bounding boxes.
+
+		Args:
+			bbox(array): Coordinates of bounding boxes. shape `(R, 4)`
+			anchor(array): Coordinates of anchors. shape `(S, 4)`
+			img_size(tuple of ints): A tuple `H, W`, which is the height and width of an image.
+
+		Returns:
+			(array, array)
+			loc: Offsets and scales to match anchors to the ground truth bounding boxes. shape `(S, 4)`
+			label: Labels of anchors with values, 1 represents postive, 0 represents negative, -1 is ignorable.
+					shape `(S, 4)`
+		'''
+		img_H, img_W = img_size
+
+		n_anchor = len(anchor)
+		inside_index = _get_inside_index(anchor, img_H, img_W)
+		anchor = anchor[inside_index]
+		argmax_ious, label = self._create_label(inside_index, anchor, bbox)
+
+		#compute bounding box regression targets.
+		loc = bbox2loc(anchor, bbox[argmax_ious])
+
+		#map up to original set of anchors
+		label = _unmap(label, n_anchor, inside_index, fill=-1)
+		loc = _unmap(loc, n_anchor, inside_index, fill=0)
+
+		return loc, label 
+
+	def _create_label(self, inside_index, anchor, bbox):
+		#label: 1 is positive, 0 is negative, -1 don't care
+		label = np.empty((len(inside_index),),dtype=np.int32)  #np.zeros((len(inside_index),),dtype=np.int32)-1
+		label.fill(-1)
+		argmax_ious, max_ious, gt_argmax_ious = self._calc_ious(anchor, bbox, inside_index)
+		# assign negative labels first so that positive labels can overwrite them
+		label[max_ious < self.neg_iou_thresh] = 0
+
+		#positive label: in case max_iou greater than pos thresh
+		label[max_ious >= self.pos_ious_thresh] = 1
+
+		#positive label: in case for each gt, anchor with highest iou
+		label[gt_argmax_iou] = 1
+
+		#subsample 
+		n_pos = int(self.pos_ratio*self.n_sample)
+		pos_index = np.where(label == 1)[0]
+		if len(pos_index) > n_pos:
+			disable_index = np.random.choice(pos_index, size=(len(pos_index)-n_pos), replace=False)
+			label[disable_index] = -1  #don't care
+
+		n_neg = self.n_sample - n_pos
+		neg_index = np.where(label == 0)[0]
+		if len(neg_index) > n_neg:
+			disable_index = np.random.choice(neg_index, size=(len(neg_index)-n_neg), replace=False)
+			label[disable_index] = -1
+
+		return argmax_ious, label 
+
+
+	def _calc_ious(self, anchor, bbox, inside_index):
+		#ious between the anchors and the gt boxes
+		ious = bbox_iou(anchor, bbox)
+		argmax_ious = ious.argmax(axis=1)
+		max_ious = ious[np.arange(len(inside_index)), argmax_ious]
+		gt_argmax_ious = ious.argmax(axis=0)
+		gt_max_ious = ious[gt_argmax_ious, np.arange(ious.shape[1])]
+		gt_argmax_ious = np.where(ious==gt_max_ious)[0]
+
+		return argmax_ious, max_ious, gt_argmax_ious
+
+
+
+
+def _get_inside_index(anchor, H, W):
+	#Calc indicies of anchors which are located completely inside of the image
+	#whose size is speficied.
+	index_inside = np.where(
+		(anchor[:, 0] >= 0)&
+		(anchor[:, 1] >= 0)&
+		(anchor[:, 2] <= H)&
+		(anchor[:, 3] <= W))[0]
+	return index_inside
+
+
+
+def _unmap(data, n_anchor, inside_index, fill=0):
+	#unmap those inside_index anchors to original set.
+	if len(data.shape) == 1:
+		#label
+		ret = np.empty((n_anchor, ), dtype=data.dtype)  #python tuple add operation
+ 		ret.fill(fill)
+		ret[inside_index] = data
+	else:
+		#loc
+		ret = np.empty((n_anchor, ) + data.shape[1: ], dtype=data.dtype)
+		ret.fill(fill)
+		ret[inside_index, :] = data
+
+	return ret
