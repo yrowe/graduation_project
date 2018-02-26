f9758356b351040afca7eeb550b18f588c468578
yrowe <912431483@qq.com>
2018-02-26 21:50:33 +0800

    testDataset

--- fasterRCNN/data/dataset.py
+++ fasterRCNN/data/dataset.py
@@ -10,13 +10,25 @@ from utils.config import opt
 class Dataset:
 	def __init__(self, opt):
 		self.opt = opt
-		self.db = VOCBboxDataset(opt.voc_dataset)
+		self.db = VOCBboxDataset(opt.voc_data_dir)
 		self.tsf = Transform(opt.min_size, opt.max_size)
 
 	def __getitem__(self, idx):
 		ori_img, bbox, label = self.db.get_example(idx)
 		img, bbox, label, scale = self.tsf((ori_img, bbox, label))
 		return img.copy(), bbox.copy(), label.copy(), scale 
+
+	def __len__(self):
+		return len(self.db)
+
+class TestDataset:
+	def __init__(self, opt):
+		self.opt = opt
+		self.db = VOCBboxDataset(opt.voc_data_dir, split='test')
+	def __getitem__(self, idx):
+		ori_img, bbox, label, difficult = self.db.get_example(idx)
+		img = preproess(ori_img)
+		return img, ori_img.shape[1:], bbox, label
 
 	def __len__(self):
 		return len(self.db)
