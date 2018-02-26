f02c0252be64505f6fe6ceeaf41ebe80ec777fdf
yrowe <912431483@qq.com>
2018-02-26 21:51:39 +0800

    eval method

--- fasterRCNN/train.py
+++ fasterRCNN/train.py
@@ -4,6 +4,7 @@ import torch
 from torch
 from torch.autograd import Variable
 from torch.utils import data as data_
+from tqdm import tqdm 
 
 from data.dataset import Dataset, TestDataset, inverse_normalize
 from utils.config import opt
@@ -11,9 +12,70 @@ from model import FasterRCNNVGG16
 from utils import array_tool as at 
 from utils.vis_tool import visdom_bbox
 from utils.eval_tool import eval_detection_voc
+from trainer import FasterRCNNVGGTrainer
 
 def train(**kwargs):
 	opt._parse(kwargs)
 	dataset = Dataset(opt)
-	
-	
+	print('load data')
+
+	dataloader = data_.DataLoader(dataset, 
+								  batch_size=1,
+								  shuffle=True,
+								  num_workers=opt.num_workers)
+	testset = TestDataset(opt)
+	test_dataloader = data_.DataLoader(testset,
+									   batch_size=1,
+									   num_workers=opt.num_workers,
+									   shuffle=False
+									   )
+
+	faster_rcnn = FasterRCNNVGG16()
+	print('model construct completed')
+
+	trainer = FasterRCNNVGGTrainer(faster_rcnn).cuda()
+
+	if opt.load_path:
+		trainer.load(opt.load_path)  # !TODO
+		print('load pre-trained model')  
+
+	best_map = 0
+	lr_ = opt.lr
+
+
+	for epoch in range(opt.epoch):
+		trainer.reset_meters()
+		for ii, (img, bbox_, label_, scale) in tqdm(enuerate(dataloader)):
+			scale = at.scalar(scale)
+			img, bbox, label = img.cuda().float(), bbox_cuda(), label_.cuda()
+			img, bbox, label = Variable(img), Variable(bbox), Variable(label)
+
+			trainer.train_step(img, bbox, label, scale)
+
+		eval_result = eval(test_dataloader, faster_rcnn, test_num=opt.test_num)
+
+
+
+
+def eval(dataloader, faster_rcnn, test_num=10000):
+	pred_bboxes, pred_labels, pred_scores = list(), list(), list()
+	gt_bboxes, gt_labels = list(), list()
+
+	for ii, (imgs, sizes, gt_bboxes_, gt_labels_) in tqdm(enumerate(dataloader)):
+		sizes = [sizes[0][0], sizes[1][0]]
+		pred_bboxes_, pred_labels, pred_scores_ = faster_rcnn.predict(imgs, [sizes])
+		gt_bboxes += list(gt_bboxes_.numpy())
+		gt_labels += list(gt_labels_.numpy())
+
+		pred_bboxes += pred_bboxes_
+		pred_labels += pred_labels_
+		pred_scores += pred_scores_
+		if ii == test_num: break
+
+	result = eval_detection_voc(
+				pred_bboxes,
+				pred_labels,
+				pred_scores,
+				gt_bboxes,
+				gt_labels,
+				use_07_metric=True)
