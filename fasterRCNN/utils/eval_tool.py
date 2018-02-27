85aaa8f4a360d38bb5a16ef87b319f01d766802e
wrc <912431483@qq.com>
2018-02-27 16:37:36 +0800

    eval tool

--- fasterRCNN/utils/eval_tool.py
+++ fasterRCNN/utils/eval_tool.py
@@ -57,6 +57,10 @@ def eval_detection_voc(
 		pred_bboxes, pred_labels, pred_scores,
 		gt_bboxes, gt_labels, iou_thresh=iou_thresh)
 
+	ap = calc_detection_voc_ap(prec, rec, use_07_metric=use_07_metric)
+
+	return {'ap': ap, 'map':np.nanmean(ap)}
+
 
 
 def calc_detection_voc_prec_rec(pred_bboxes, pred_labels, pred_scores,
@@ -115,3 +119,81 @@ def calc_detection_voc_prec_rec(pred_bboxes, pred_labels, pred_scores,
 			del iou
 
 			selec = np.zeros(gt_bbox_l.shape[0], dtype=bool)
+			for gt_idx in gt_index:
+				if gt_idx >= 0:
+					if gt_difficult_l[gt_idx]:
+						match[1].append(-1)
+					else:
+						if not selec[gt_idx]:
+							match[1].append(1)
+						else:
+							match[1].append(0)
+				selec[gt_idx] = True
+
+			else:
+				match[1].append(0)
+
+	for iter_ in (pred_bboxes, pred_labels, pred_scores, gt_bboxes, gt_labels, gt_difficults):
+		if next(iter_, None) is not None:
+			raise ValueError('Length of input iterables need to be same')
+
+	n_fg_class = max(n_pos.keys()) + 1
+	prec = [None]*n_fg_class
+	rec = [None]*n_fg_class
+
+	for l in n_pos.keys():
+		score_l = np.array(score[l])
+		match_l = np.array(match[l], dtype=np.int8)
+
+		order = score_l.argsort()[::-1]
+		match_l = match_l[order]
+
+		tp = np.cumsum(match_l == 1)
+		fp = np.cumsum(match_l == 0)
+
+		# If an element of fp+tp is 0,
+		# the corresponding element of prec[1] is nan.
+
+		prec[l] = tp/(fp + tp)
+		#If n_pos[l] is 0, rec[l] is None
+		if n_pos[l] > 0:
+			rec[l] = tp / n_pos[l]
+
+	return prec, rec 
+
+
+def calc_detection_voc_ap(prec, rec, use_07_metric=False):
+
+    n_fg_class = len(prec)
+    ap = np.empty(n_fg_class)
+    for l in range(n_fg_class):
+        if prec[l] is None or rec[l] is None:
+            ap[l] = np.nan
+            continue
+
+        if use_07_metric:
+            # 11 point metric
+            ap[l] = 0
+            for t in np.arange(0., 1.1, 0.1):
+                if np.sum(rec[l] >= t) == 0:
+                    p = 0
+                else:
+                    p = np.max(np.nan_to_num(prec[l])[rec[l] >= t])
+                ap[l] += p / 11
+        else:
+            # correct AP calculation
+            # first append sentinel values at the end
+            mpre = np.concatenate(([0], np.nan_to_num(prec[l]), [0]))
+            mrec = np.concatenate(([0], rec[l], [1]))
+
+            mpre = np.maximum.accumulate(mpre[::-1])[::-1]
+
+            # to calculate area under PR curve, look for points
+            # where X axis (recall) changes value
+            i = np.where(mrec[1:] != mrec[:-1])[0]
+
+            # and sum (\Delta recall) * prec
+            ap[l] = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+
+    return ap
+
