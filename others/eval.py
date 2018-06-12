bd4b9aea386015cdb443d53b647f29fe9e1e179b
wrc <912431483@qq.com>
2018-06-13 00:57:27 +0800

    final version

--- others/eval.py
+++ others/eval.py
@@ -3,6 +3,8 @@ from ipdb import set_trace
 import os
 import pandas as pd
 
+#when calculate ssd or yolo, just change the corresponding path.
+
 def bbox_iou(box1, box2):
     ax1, ax2, ay1, ay2 = box1
     bx1, bx2, by1,by2 = box2
@@ -20,23 +22,90 @@ def bbox_iou(box1, box2):
     iou = inter_area /(a_area + b_area - inter_area)
     return iou
 
-def compute_iou():
-    #predictPath = "F:\PASCAL_VOC\VOCdevkit\VOC2007\yolopredict"
-    #predictPath = "F:\PASCAL_VOC\VOCdevkit\VOC2007\HoGpredict"
-    #predictPath = "F:/PASCAL_VOC_LINUX/PASCAL_VOC/VOCdevkit/VOC2007/ssdpredict"
+def compute_predict():
     predictPath = "F:/PASCAL_VOC_LINUX/PASCAL_VOC/VOCdevkit/VOC2007/rcnnpredict"
     labelPath = "F:\PASCAL_VOC\VOCdevkit\VOC2007\labels"
-    savePath = "F:\PASCAL_VOC\VOCdevkit\VOC2007\yoloIOU"
+    savePath = "F:/PASCAL_VOC_LINUX/PASCAL_VOC/VOCdevkit/VOC2007/rcnnIOU"
 
-    precisionPath = "F:\graduation_project\yolo_precision.txt"
+    precisionPath = "F:/graduation_project/rcnn_precision"
+    recallPath = "F:/graduation_project/rcnn_recall"
 
     totalFile = os.listdir(predictPath)
+    for fileName in totalFile:
+        file = predictPath + '/' +fileName
+        gtfile = labelPath + '/' + fileName
+        if os.path.getsize(file) == 0:
+            #maybe when calculate recall, it shouldnot be skipped.
+            #outp = open(savePath+'/'+fileName, "w")
+            continue
 
-    #yolo predict may be empty
-    #but if only yolo predict has some prediction, 
-    #there must be corresponding label for ground truth.
-    locatingDis = 0
-    num = 0
+        f = open(file)
+        line = f.readline()
+        bbox1 = []
+        while line:
+            line = line.replace("\n", "")
+            line = line.split(" ")[1:5]
+            line = [int(float(i)) for i in line]
+            bbox1.append(line)
+            line = f.readline()
+        f.close()
+        bbox1 = np.array(bbox1)
+        
+        f = open(gtfile)
+        line = f.readline()
+        bbox2 = []
+        while line:
+            line = line.replace("\n", "")
+            line = line.split(" ")[1:5]
+            line = [int(float(i)) for i in line]
+            bbox2.append(line)
+            line = f.readline()
+        f.close()
+        bbox2 = np.array(bbox2)
+
+        len1 = len(bbox1)
+        len2 = len(bbox2)
+
+        mat = np.ones((len1, len2))
+
+        for i in range(len1):
+            for j in range(len2):
+                iou = bbox_iou(bbox1[i], bbox2[j])
+                mat[i][j] = iou
+        df = pd.DataFrame(mat)
+        threshList = [0.2,0.3,0.4,0.5]
+        for thresh in threshList:
+            df = pd.DataFrame(mat)
+            totalPredictNum = df.index.shape[0]
+            totalgtNum = df.columns.shape[0]
+            truePredictNum = 0
+            for i in range(df.columns.shape[0]):
+                tmpIndex = df[i].argmax()
+                if df[i][tmpIndex] > thresh:
+                    truePredictNum += 1
+
+                df = df.drop(tmpIndex)
+                if df.index.shape[0] == 0:
+                    break
+            precision = truePredictNum/totalPredictNum
+            recall = truePredictNum/totalgtNum
+
+            #add mode is "write".
+            with open(precisionPath+"{}.txt".format(thresh), "a+") as fp:
+                fp.write(str(precision)+'\n')
+
+            with open(recallPath+"{}.txt".format(thresh), "a+") as fp:
+                fp.write(str(recall)+'\n')
+
+def compute_locate():
+    predictPath = "F:/PASCAL_VOC_LINUX/PASCAL_VOC/VOCdevkit/VOC2007/rcnnpredict"
+    labelPath = "F:\PASCAL_VOC\VOCdevkit\VOC2007\labels"
+    savePath = "F:/PASCAL_VOC_LINUX/PASCAL_VOC/VOCdevkit/VOC2007/rcnnIOU"
+
+    locatePath = "F:/graduation_project/rcnn_locate.txt"
+
+    locatingDis = 0  #total distance for the whole dataset.
+    num = 0  # num of valid detections. 
     for fileName in totalFile:
         file = predictPath + '/' +fileName
         gtfile = labelPath + '/' + fileName
@@ -85,60 +154,14 @@ def compute_iou():
         for i in range(len2):
             tmpIndex = df[i].argmax()
             if df[i][tmpIndex] > 0.2:
-                try:
-                    locatingDis += np.abs(bbox1[tmpIndex] - bbox2[i]).sum()
-                    num += 1
-                except:
-                    set_trace()
-        #set_trace()
+                locatingDis += np.abs(bbox1[tmpIndex] - bbox2[i]).sum()
+                num += 1
 
-        '''
-        threshList = [0.2,0.3,0.4,0.5]
-        for thresh in threshList:
+        with open(locatePath, "w") as fp:
+            fp.write(str(locatingDis/num))
 
-            df = pd.DataFrame(mat)
-            totalPredictNum = df.index.shape[0]
-            truePredictNum = 0
-            for i in range(df.columns.shape[0]):
-                tmpIndex = df[i].argmax()
-                if df[i][tmpIndex] > thresh:
-                    truePredictNum += 1
-
-                df = df.drop(tmpIndex)
-                if df.index.shape[0] == 0:
-                    break
-
-            precision = truePredictNum/totalPredictNum    
-            
-            with open(precisionPath+"{}".format(thresh),"a+") as fp:
-                fp.write(str(precision)+'\n')
-        '''
-        #outp = open(savePath+'/'+fileName,"w")
-        #outp.write(ans)
-    print(locatingDis)
-    print(num)
-
-def cntp_precsion(path = "F:\graduation_project\yolo_precision.txt"):
-    df = pd.read_csv("precision.txt", header=None)
-    print(df.mean())
 
 if __name__ == '__main__':
-    compute_iou()
-
-'''      
-a = [[185, 279, 62, 199]]
-b = [[177, 288, 63, 175]]
-
-len1 = len(a)
-len2 = len(b)
-
-ans = np.ones((len1, len2))
-
-for i in range(len1):
-    for j in range(len2):
-        iou = bbox_iou(a[i], b[j])
-        ans[i][j] = iou
-
-print(ans)
-#set_trace()
-'''
+    compute_predict()
+    compute_locate()
+    
